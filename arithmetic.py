4f5c17b3ffd8604e1b68f3f512289968449c0764
Veronica Hall <86692664+ver0nicahall@users.noreply.github.com>
2022-03-30 13:34:25 -0500

    Finished divide, square, and cube

--- arithmetic.py
+++ arithmetic.py
@@ -22,18 +22,24 @@ def multiply(num1, num2):
 def divide(num1, num2):
     """Divide the num1 by num2, returning a floating point."""
 
+    return num1 / num2
+
 
 def square(num1):
     """Return the square of num1."""
-
+    
+    return num1 ** 2
 
 def cube(num1):
     """Return the cube of num1."""
+
+    return num1 ** 3
 
 
 def power(num1, num2):
     """Raise num1 to the power of num2 and return the value."""
 
+    return num1 ** num2
 
 def mod(num1, num2):
     """Return the remainder of num1 / num2."""
